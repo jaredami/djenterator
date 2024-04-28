67d0f81f59abfd4ef75a9651b6ad0d86038cc292
Jared Storm <jaredrstorm@gmail.com>
2024-04-28 17:54:19 -0400

    Basic styles for volume controls

--- src/components/VolumeControl.tsx
+++ src/components/VolumeControl.tsx
@@ -1,9 +1,11 @@
+import './VolumeControl.scss';
+
 export const VolumeControl: React.FC<{
   label: string;
   value: number;
   onChange: (newVolume: number) => void;
 }> = ({ label, value, onChange }) => (
-  <>
+  <div className="volume-control">
     <label style={{ color: 'white' }}>{label}</label>
     <input
       type="range"
@@ -12,5 +14,5 @@ export const VolumeControl: React.FC<{
       value={value}
       onChange={(e) => onChange(Number(e.target.value))}
     />
-  </>
+  </div>
 );
